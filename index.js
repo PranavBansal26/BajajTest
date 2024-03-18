c8f12d05c5290199ea744c99807942d3f795cf8a
PranavBansal26 <102216552+PranavBansal26@users.noreply.github.com>
2024-03-18 11:30:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+const response = {
+    is_success: true,
+    user_id: "pranav_bansal_26092003", 
+    email:'pranav2341.be21@chitkara.edu.in',
+    roll_number:'2110992341',
+    odd_numbers: [],
+    even_numbers: [],
+    alphabets: []
+}
+
+app.post('/bfhl',(req,res) => {
+    for(let i=0;i<req.body.length;i++){
+        if(typeof req.body[i] === 'number'){
+            if(req.body[i]%2==0){
+                response.even_numbers.add(req.body[i]);
+            }
+            else{
+                response.odd_numbers.add(req.body[i]);
+            }
+
+        }
+        else if(typeof req.body[i] == 'string'){
+            response.alphabets.add(req.body[i]);
+
+        }
+    }
+    return res.status(200).json(response);
+})
+
+app.listen(5000, ()=>{
+    console.log('hi')
+})
+
